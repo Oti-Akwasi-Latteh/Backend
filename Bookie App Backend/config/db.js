7e125c72828ad68ed0138b1c33c583b16f5cf9c4
Oti-Akwasi-Latteh <166330540+Oti-Akwasi-Latteh@users.noreply.github.com>
2026-04-07 17:48:01 -0800

    Update db.js

--- Bookie App Backend/config/db.js
+++ Bookie App Backend/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
  
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI || "mongodb+srv://quasilyphae:ZEzy3qvdJOKOYnYQ@cluster0.aj3thcq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB connected");
   } catch (err) {
     console.error("MongoDB connection error:", err.message);
